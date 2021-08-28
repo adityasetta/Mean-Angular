602d8325ce7150be83ad083d3b34a68c8605d75d
Aditya Parama Setta <adityasetta@gmail.com>
2021-08-28 21:38:47 +0700

    add rest api sample

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-  console.log('fist middleware');
-  next();
-});
-
-app.use((req,res,next) => {
-  res.send('Hello from express!');
+app.use( '/api/posts', (req,res,next) => {
+  const posts = [
+    { id: 'asdf123', title: 'First', content: 'first content' },
+    { id: 'asdf125', title: 'Second', content: 'second content' },
+    { id: 'asdf126', title: 'Third', content: 'third content' }
+  ];
+  res.status(200).json({
+    message: 'Posts Success!',
+    posts: posts
+  });
 });
 
 module.exports = app;
