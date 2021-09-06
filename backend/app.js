54712a7085b783027c131cd6d8c03b7899f50a03
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-06 12:42:42 +0700

    connect node to angular

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
   next();
 });
 
-app.use( '/api/posts', (req,res,next) => {
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added!'
+  });
+});
+
+app.get( '/api/posts', (req,res,next) => {
   const posts = [
     { id: 'asdf123', title: 'First', content: 'first content' },
     { id: 'asdf125', title: 'Second', content: 'second content' },
