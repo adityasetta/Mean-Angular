01aae42f9934db2e1974475c7db3dff01b63af98
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-06 12:51:35 +0700

    Add mongoose model for posting modeling

---
+++ backend/models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  title: {
+          type: String,
+          required: true
+         }, // read more in mongoose docs : https://mongoosejs.com/docs/guide.html
+  content: {
+          type: String,
+          required: true
+         }
+});
+
+module.exports = mongoose.model('Post', postSchema);
