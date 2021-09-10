9d8165a8074fb977beede0239675c1c22f2ef82f
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-10 08:30:23 +0700

    Pagination : add component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
 import { Subscription } from "rxjs";
 
 import { Post } from "../post.model";
@@ -17,6 +18,9 @@ export class PostListComponent implements OnInit, OnDestroy{
   // ];
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   // Constructor
@@ -35,6 +39,10 @@ export class PostListComponent implements OnInit, OnDestroy{
         this.isLoading = false;
         this.posts = posts;
       });
+  }
+
+  onChangedPage(pageData: PageEvent){
+    console.log(pageData);
   }
 
   onDelete(postId: string){
