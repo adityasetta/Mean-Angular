61d4874ad9f221df4a9c43507cd8b8508ffaa74d
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-12 12:26:00 +0700

    Add authentication by jwt token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -24,6 +26,39 @@ router.post("/signup", (req, res, next) => {
             error: err
           });
         });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user){
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if(!result){
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+        'secret_id',
+        {expiresIn: "1h"}
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
 });
 
