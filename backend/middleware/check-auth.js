5d87f26df2ead9280ae033bb3f11d8c7cf07bd55
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-15 11:39:54 +0700

    add user data object to post object

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next) => {
   try{
     // take the second string value after Bearer token
     const token = req.headers.authorization.split(" ")[1];
-    console.log("entering interceptor");
-    jwt.verify(token, "secret_id_sample_should_be_like_ea38456fhg");
+    const decodedToken = jwt.verify(token, "secret_id_sample_should_be_like_ea38456fhg");
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   }
   catch(err){
