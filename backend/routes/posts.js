ce05135dc29ba2f99335997ec595c26753380bb0
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-16 19:13:04 +0700

    Adding Controller to separate code
    and fix post create

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const multer = require("multer");
 
-const Post = require('../models/post');
 const checkAuth = require("../middleware/check-auth");
+const PostController = require("../controllers/post");
 
 const router = express.Router();
 
@@ -31,129 +31,17 @@ const storage = multer.diskStorage({
 router.post(
   "",
   checkAuth,
-  multer({storage: storage}).single("image"), (req, res, next) => {
-  const url = req.protocol + '://' + req.get("host");
-  console.log('entering multer');
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save().then(result => {
-    console.log('yellow');
-    res.status(201).json({
-      message: 'Post added!',
-      post: {
-        ...result, // get and copy all value from result
-        id: result._id,
-        // may also use these
-        // title: result.title,
-        // content: result.content,
-        // imagePath: result.imagePath
-      }
-    });
-  })
-  .catch(err=>{
-    res.status(500).json({
-      message: "Create a post failed"
-    })
-  });
-});
+  multer({storage: storage}).single("image"), PostController.createPost);
 
-router.get("", (req,res,next) => {
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-
-  if(pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
-  }
-  postQuery
-    .then(documents=>{
-      fetchedPosts = documents;
-      return Post.count();
-    })
-    .then(count=>{
-      res.status(200).json({
-        message: 'Posts Success!',
-        posts: fetchedPosts,
-        maxPosts: count
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Fetching post failed!"
-      });
-    });
-});
+router.get("", PostController.getPosts);
 
 router.put(
   "/:id",
   checkAuth,
-  multer({storage: storage}).single("image"), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if(req.file){
-    const url = req.protocol + '://' + req.get("host");
-    imagePath = url + "/images/" + req.file.filename
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-    creator: req.userData.userId
-  });
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId },post).then(result => {
-    if( result.nModified>0 ){
-      res.status(200).json({message: "Post updated!"});
-    }
-    else{
-      res.status(401).json({message: "Not Authorized"});
-    }
-  })
-  .catch(error =>{
-    res.status(500).json({
-      message: "Couldn't update post"
-    });
-  });
-});
+  multer({storage: storage}).single("image"), PostController.updatePost);
 
-router.get("/:id", (req, res, next) =>{
-  Post.findById(req.params.id).then(post=>{
-    if(post){
-      res.status(200).json(post);
-    }
-    else{
-      res.status(404).json({message:'Post not found!'});
-    }
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: "Fetching post failed!"
-    });
-  });
-});
+router.get("/:id", PostController.getPost);
 
-router.delete("/:id", checkAuth, (req,res,next)=>{
-  Post.deleteOne({_id: req.params.id, creator: req.userData.userId}).
-    then(result => {
-      //console.log(result); //this to check parameter to indicate success/failed
-      if( result.n > 0 ){
-        res.status(200).json({message: "Post deleted!"});
-      }
-      else{
-        res.status(401).json({message: "Not Authorized"});
-      }
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Delete post failed!"
-      });
-    });
-});
+router.delete("/:id", checkAuth, PostController.deletePost);
 
 module.exports = router;
