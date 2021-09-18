c35286d051651a435971c6c4840b6bab8fa34b1c
Aditya Parama Setta <adityasetta@gmail.com>
2021-09-18 13:27:58 +0700

    Global variable : manage angular var

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -5,6 +5,9 @@ import { map } from 'rxjs/operators';
 
 import { Post } from "./post.model";
 import { Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+
+const BACKEND_URL = environment.apiUrl + "/posts/";
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
@@ -15,7 +18,7 @@ export class PostsService{
 
   getPosts(postsPerPage: number, currentPage: number){
     const queryParams = `?pageSize=${postsPerPage}&page=${currentPage}`;
-    this.httpClient.get<{message: string, posts: any, maxPosts: number}>('http://localhost:3000/api/posts' + queryParams)
+    this.httpClient.get<{message: string, posts: any, maxPosts: number}>(BACKEND_URL + queryParams)
       .pipe(map((postData)=>{
         return {  posts: postData.posts.map(post => {
           return{
@@ -50,7 +53,7 @@ export class PostsService{
     postData.append("content", content);
     postData.append("image", image, title);
 
-    this.httpClient.post<{message: string, post: Post }>('http://localhost:3000/api/posts', postData)
+    this.httpClient.post<{message: string, post: Post }>(BACKEND_URL, postData)
       .subscribe((responseData) => {
         this.router.navigate(["/"]);
       });
@@ -74,7 +77,7 @@ export class PostsService{
         creator: null
       };
     }
-    this.httpClient.put("http://localhost:3000/api/posts/" + id, postData)
+    this.httpClient.put(BACKEND_URL + id, postData)
       .subscribe(response =>
         {
           this.router.navigate(["/"]);
@@ -82,6 +85,6 @@ export class PostsService{
   }
 
   deletePost(postId: string){
-    return this.httpClient.delete("http://localhost:3000/api/posts/" + postId);
+    return this.httpClient.delete(BACKEND_URL + postId);
   }
 }
